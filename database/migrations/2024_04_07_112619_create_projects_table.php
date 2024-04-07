f6314000c5da26a8e527b8ac3ff5230dada74e5f
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-04-07 13:51:08 +0200

    created migration for porjects table

--- database/migrations/2024_04_07_112619_create_projects_table.php
+++ database/migrations/2024_04_07_112619_create_projects_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('url');
+            $table->date('creation_data');
+            $table->boolean('status');
+            $table->string('note');
             $table->timestamps();
         });
     }
