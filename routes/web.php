b7b5dc22a51810ec308e4ed48bb0473b7d2f9a92
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-04-09 18:21:56 +0200

    added route for project resource in middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::resource('/projects', [ProjectController::class]);
+    });
